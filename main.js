0558abc0475c960e17cb012997006f3a9cb5e5ad
ecowatt-elec <ecowattfuture@gmail.com>
2025-04-21 13:31:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -32,7 +32,9 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
 
-    // ===== Mobile Menu Toggle =====
+
+
+    
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close');
@@ -50,6 +52,20 @@ if (navClose) {
     navMenu.classList.remove('show-menu');
   });
 }
+
+
+    const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () => {
+  const navMenu = document.getElementById('nav-menu');
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show-menu');
+};
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+    
 
     // ===== Fade-in Sections on Scroll =====
     const fadeSections = document.querySelectorAll(".fade-in");
