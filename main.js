0cc825f08de755fd2aaf9deb5e048d1a4364a7da
ecowatt-elec <ecowattfuture@gmail.com>
2025-04-23 12:51:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -129,24 +129,3 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-
-const scrollBtn = document.getElementById("scrollTopBtn");
-
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 100) { // Show earlier
-        scrollBtn.classList.add("show");
-    } else {
-        scrollBtn.classList.remove("show");
-    }
-});
-
-scrollBtn.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-});
-
-
-
-
